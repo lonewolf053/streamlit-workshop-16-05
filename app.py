8df0b66e32e27c91229864f7541387ac54239d8e
lonewolf053 <prime.05@tutanota.com>
2022-05-16 18:22:01 +0530

    trying out streamlit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("Hello, world")
+st.header("Hello darkness my old friend")
+st.subheader("I've come to talk with you again")
+st.text("Without the vision softly creeping")
+st.write("And the vision")
+st.success("Sound of silence")
+st.info("And in the ")
